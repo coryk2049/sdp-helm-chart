8e1ced73ef5694c3244c6e22603d5824cc070ce0
neo <cory.kin.ctr@navy.mil>
2019-06-12 16:34:42 -0400

    First commit

---
+++ sdp_prereqs.sh
@@ -0,0 +1,10 @@
+curl -L -O https://git.io/get_helm.sh
+sleep 5
+chmod 700 get_helm.sh
+sleep 5
+./get_helm.sh --version v2.13.0
+
+oc status
+oc login -u system:admin
+oc adm policy add-cluster-role-to-user cluster-admin admin
+oc logout
